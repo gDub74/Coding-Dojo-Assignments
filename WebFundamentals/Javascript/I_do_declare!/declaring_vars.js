85f8be657805d5739db112b4429897a3b60afc27
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-11 13:08:55 -0800

    edited the about file and the undefined variable decleration.

--- WebFundamentals/Javascript/I_do_declare!/declaring_vars.js
+++ WebFundamentals/Javascript/I_do_declare!/declaring_vars.js
@@ -21,21 +21,24 @@ var school = "Coding Dojo"
  //undefined variable either doesn't have a value in the decleration or an unmatching pair of quotes like - ' "
 // var undefined = ;
 // var headAche = 'ouch";
+ var noVar = undefined;
 
 
-  //print the variables 
+//print the variables 
 
-  console.log("this is a number:", num);
-  console.log("I am this old:", myAge, " years");
-  console.log("I was born in:", birthYear);
-  console.log("Example of a negative number:", negativeNum);
-  
-  console.log("Hi, my name is:", myName);
-  console.log("I grew up in:", homeTown);
-  console.log("I am currently enrolled in the aweome school called:", school);
-  
-  console.log("I am stoked on my school:",imStoaked);
-  console.log("I'm boared with what I'm learning:", imBoared);
+console.log("this is a number:", num);
+console.log("I am this old:", myAge, " years");
+console.log("I was born in:", birthYear);
+console.log("Example of a negative number:", negativeNum);
 
+console.log("Hi, my name is:", myName);
+console.log("I grew up in:", homeTown);
+console.log("I am currently enrolled in the aweome school called:", school);
+
+console.log("I am stoked on my school:",imStoaked);
+console.log("I'm boared with what I'm learning:", imBoared);
+
+
+console.log("Example of an undefined variable:",noVar);
 //   console.log("calling a variable that hasn't been defined", undefined);
   

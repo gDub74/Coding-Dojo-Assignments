dcf1e58fc8beb6ea1c98455950bb1d58411ad4cc
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-11 13:02:24 -0800

    assignment 1 on declaring number, string and boolean variables.

---
+++ WebFundamentals/Javascript/I_do_declare!/declaring_vars.js
@@ -0,0 +1,41 @@
+
+
+
+// declaring number vsriables
+var num = 7;
+var myAge = 43;
+var birthYear = 1974;
+var negativeNum = -26;
+
+
+// string variables
+var myName = 'Greg'
+var homeTown = "Santa Cruz, Ca"
+var school = "Coding Dojo"
+
+
+// Booleans
+ var imStoaked = true;
+ var imBoared = false;
+
+ //undefined variable either doesn't have a value in the decleration or an unmatching pair of quotes like - ' "
+// var undefined = ;
+// var headAche = 'ouch";
+
+
+  //print the variables 
+
+  console.log("this is a number:", num);
+  console.log("I am this old:", myAge, " years");
+  console.log("I was born in:", birthYear);
+  console.log("Example of a negative number:", negativeNum);
+  
+  console.log("Hi, my name is:", myName);
+  console.log("I grew up in:", homeTown);
+  console.log("I am currently enrolled in the aweome school called:", school);
+  
+  console.log("I am stoked on my school:",imStoaked);
+  console.log("I'm boared with what I'm learning:", imBoared);
+
+//   console.log("calling a variable that hasn't been defined", undefined);
+  

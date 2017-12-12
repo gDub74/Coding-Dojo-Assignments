af3e98461425aac801f8c8344420b2ee85405509
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-11 19:26:38 -0800

    learned shorter way to write an && contitional so changed it here on the else if statment

--- WebFundamentals/Javascript/While You Wait/BirthdayCountdown.js
+++ WebFundamentals/Javascript/While You Wait/BirthdayCountdown.js
@@ -7,7 +7,7 @@ for (var daysUntilMyBirthday = 60; daysUntilMyBirthday > 0; daysUntilMyBirthday 
     if (daysUntilMyBirthday > 30) {
         console.log("I have to wait", daysUntilMyBirthday, "until my birthday. That is soooooo long, ugh!");
     }
-    else if (daysUntilMyBirthday <= 30 && daysUntilMyBirthday > 5) {
+    else if (daysUntilMyBirthday <= 30 > 5) {
         console.log("It's getting closer to my Birthday-only", daysUntilMyBirthday, "days left! :)");
     }
     else {

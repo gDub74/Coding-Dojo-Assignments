3c54068755ab67f1b912e4fe837d07e29e4edb03
Greg Weber <gregoryweberguitars@gmail.com>
2017-12-17 16:51:17 -0800

    cloned a github branch and debugged the jQuery in it. Also added some changes to the Ninja-Cat assignment to allow a temp atribute to be used in the image elements as a way to use the attribute jQuery method to toggle between two images.

--- WebFundamentals/Javascript/Ninja_Cat/script.js
+++ WebFundamentals/Javascript/Ninja_Cat/script.js
@@ -3,8 +3,8 @@
 $('document').ready(function(){
 
     $('img').click(function(){
-        console.log($(this).attr('src'));
-        $(this).attr('src', $(this).attr('data-alt-src'));
-        console.log('now', $(this).attr('src'));
+        $(this).attr('temp', $(this).attr('src'));  //sets an empty temp attribute to the value of the src attribute
+        $(this).attr('src', $(this).attr('data-alt-src')); //sets the src to the alt-src
+        $(this).attr('data-alt-src', $(this).attr('temp')); //resets the alt-src to what the src was originally
     })
 })

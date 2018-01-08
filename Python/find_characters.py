7ee64e4bbd3fdf2fa373552a3728c3c08988d10b
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-07 20:57:32 -0800

    changed some variable names to make more readable

--- Python/find_characters.py
+++ Python/find_characters.py
@@ -1,23 +1,31 @@
 # Write a program that takes a list of strings and a string containing a single character, and prints a new list of all the strings containing that character.
 
 
-#test case
-
+#test cases
 word_list = ['hello','world','my','name','is','Anna']
 char = 'o'
-new_list= []
+char2 = "m"
+char3 = "n"
+subStr1 = "ll"   #works for multi-character substrings, not just single characters 
 
 
-def characterChecker(list, value):
-    #loop through elements in list and compare with value to see if value is in the element
-    for element in list:
-        if value in element:
-            new_list.append(element)
+
+
+def characterChecker(my_list, subString):
+    new_list= []
+    #loop through elements in list and compare with subString value to see if it's in each element
+    for element in my_list:
+        if subString in element:   
+            new_list.append(element)   #if it is then append the element to the new list
 
     print new_list
 
 
 characterChecker(word_list, char)
+characterChecker(word_list, char2)
+characterChecker(word_list, char3)
+characterChecker(word_list, subStr1)
+
 
 
 

00ae53fc513e47adc0202af11ba1d5df3f539539
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-07 21:36:49 -0800

    changed the print format to allow for printing of red characters in the terminal output.

--- Python/checkerboard.py
+++ Python/checkerboard.py
@@ -1,7 +1,9 @@
-#print a patern to the console
+#print a red checkerboard patern to the console
+
 for count in range (0, 4):
-    print '* * * *'
-    print ' * * * *'
+    print '\033[1;31m* * * *\033[1;m'  #found formating for red output in terminal via google search
+    print '\033[1;31m * * * *\033[1;m'
+    
 
 
 

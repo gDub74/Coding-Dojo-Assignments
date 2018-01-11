528be30054e0098ae74cb7ef54d6a4f0759f46e9
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-10 16:36:41 -0800

    function to display *'s depending on inputed list

---
+++ Python/stars.py
@@ -0,0 +1,12 @@
+#Create a function called draw_stars() that takes a list of numbers and prints out *'s.
+
+
+def starPrinter(list):
+    for element in list:
+        print '\033[1;31m *\033[1;m' * element  #red formating with '*' - and * is used as a multiplier after the ''
+
+starPrinter([1,2,3])
+starPrinter([4,5,6])
+starPrinter([6,5,4,3,2,11])
+
+

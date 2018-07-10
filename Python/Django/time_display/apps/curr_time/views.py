0b3d2bf3d64e5533a0210d686ed0789261c41592
Greg Weber <gregoryweberguitars@gmail.com>
2018-07-10 14:20:13 -0700

    playing witht the timezone

--- Python/Django/time_display/apps/curr_time/views.py
+++ Python/Django/time_display/apps/curr_time/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime, localtime
+from datetime import datetime
+from pytz import timezone
+
+ptz = timezone('US/Pacific')
+local_time = datetime.now(ptz) - 7
 
 def index(request):
   context = {
-  "time": strftime("%b %d, %Y %I:%M %p", localtime())
+  # "time": strftime("%b %d, %Y %I:%M %p", localtime())
+  "time": local_time
   }
   return render(request,'curr_time/index.html', context)
